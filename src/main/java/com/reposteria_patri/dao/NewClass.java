60bf4705610885419aa445204d95b1ea7432afa0
Jordy Retana <jretanamendez@gmail.com>
2024-08-14 11:41:26 -0600

    Prueba de Corregido

--- src/main/java/com/reposteria_patri/dao/NewClass.java
+++ src/main/java/com/reposteria_patri/dao/NewClass.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.reposteria_patri.domain;
+package com.reposteria_patri.dao;
 
 /**
  *
- * @author Sebastian Solano
+ * @author Jordy
  */
 public class NewClass {
     
